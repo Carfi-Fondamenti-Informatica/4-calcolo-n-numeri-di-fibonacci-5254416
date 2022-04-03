54e669bcea130bbec9fa5173d1d94c3dc6d40620
5254416 <100807837+5254416@users.noreply.github.com>
2022-04-03 18:11:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+  int n, n1=1, n2=1, n3, a=2;
+  std::cout<<"Inserire un valore intero n.\n";
+  cin >> n;
+  if (n>=2) {
+      std::cout << n1 << "\n" << n2;
+      while (a < n) {
+          n3 = n1 + n2;
+          std::cout << "\n" << n3;
+          n1 = n2;
+          n2 = n3;
+          a++;
+      }
+  } else {
+      std::cout << "errore";
+  }
+  return 0;
 }
